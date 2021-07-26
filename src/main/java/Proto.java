9da1b387e2bffe7677e834cc786815aa53004559
marty-farce <marty.farce@protonmail.com>
2021-07-26 00:46:36 -0700

    final

--- src/main/java/Proto.java
+++ src/main/java/Proto.java
@@ -1,6 +1,17 @@
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.nio.charset.StandardCharsets;
+
 public class Proto {
-    public static void main(String[] args) {
-        PersonOuterClass.Person person = PersonOuterClass.Person.newBuilder().setName("Buford Tanner").setAge(22).build();
-        System.out.println(person.toString());
+    private static byte[] encode() {
+        PersonOuterClass.Person person = PersonOuterClass.Person.newBuilder().setName("Buford").setAge(22).build();
+
+        return person.toByteArray();
+    }
+
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        System.out.println(encode().toString());
+
+        System.out.println(PersonOuterClass.Person.parseFrom(encode()));
     }
 }
